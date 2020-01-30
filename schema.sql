048aab9c3739d5863d587d0ccdf9b3c406713c48
Nicholas Chappel <nwchappel@Nicholass-MacBook-Pro.local>
2020-01-29 21:03:21 -0500

    Finalized Jupyter Notebook, Resources, and added SQL schema

---
+++ schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE school (
+	id INT PRIMARY KEY,
+	longitude float,
+	latitude float,
+	address varchar,
+	city varchar,
+	zipcode INT,
+	school_name varchar,
+	school_type varchar
+);
+
+CREATE TABLE offense_codes (
+	offense_code INT PRIMARY KEY,
+	name varchar
+);
+
+CREATE TABLE crime (
+	incident_number varchar PRIMARY KEY,
+	offense_code INT,
+	offense_code_group varchar,
+	offense_description varchar,
+	district varchar,
+	date varchar,
+	street varchar,
+	latitude float,
+	longitude float,
+	FOREIGN KEY (offense_code) REFERENCES offense_codes (offense_code)
+);
+
+CREATE TABLE housing (
+	id INT PRIMARY KEY,
+	avg_rooms_per_dwelling float,
+	percent_lower_status_population float, 
+	pupil_teacher_ratio float,
+	median_value float
+);
+
+select * from school;
+
